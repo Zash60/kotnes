7da494318e866e963dca6527b1dd9bce9689065d
Zash60 <xath06@gmail.com>
2025-11-11 10:54:10 -0300

    Update Emulator.kt

--- app/src/main/kotlin/com/zash60/kotnes/core/Emulator.kt
+++ app/src/main/kotlin/com/zash60/kotnes/core/Emulator.kt
@@ -1,12 +1,5 @@
 package com.zash60.kotnes.core
-import com.zash60.kotnes.core.apu.Apu
-import com.zash60.kotnes.core.ext.update
-import com.zash60.kotnes.core.ext.isSetUInt
-import com.zash60.kotnes.core.ext.isSetUByte
-import com.zash60.kotnes.core.ext.extract
-import com.zash60.kotnes.core.ext.toInt
 
-// IMPORTS CORRIGIDOS
 import com.zash60.kotnes.core.apu.*
 import com.zash60.kotnes.core.cartridge.Cartridge
 import com.zash60.kotnes.core.cartridge.Rom
@@ -21,6 +14,7 @@ import com.zash60.kotnes.core.ppu.Canvas
 import com.zash60.kotnes.core.ppu.Ppu
 import com.zash60.kotnes.core.ram.Ram
 import kotlin.math.round
+
 class Emulator(
     cartridge: Cartridge,
     canvas: Canvas,
@@ -32,12 +26,14 @@ class Emulator(
             write(idx, data.toUnsignedInt())
         }
     }
+
     private val ppu = Ppu(
         chrRam = chrRam,
         canvas = canvas,
         interrupts = interrupts,
         isHorizontalMirror = cartridge.isHorizontalMirror
     )
+
     private val apu = Apu(
         pulse1 = PulseChannel(
             envelopeGenerator = EnvelopeGenerator(),
@@ -45,14 +41,23 @@ class Emulator(
             isChannelOne = true,
         ),
         pulse2 = PulseChannel(
+            envelopeGenerator = EnvelopeGenerator(),
+            lengthCounter = LengthCounter(),
             isChannelOne = false,
+        ),
         triangle = TriangleChannel(
+            lengthCounter = LengthCounter(),
+        ),
         noise = NoiseChannel(
+            envelopeGenerator = EnvelopeGenerator(),
+            lengthCounter = LengthCounter(),
         )
+    )
     private val wRam = Ram(0x2048)
     private val prgRom = Rom(cartridge.program)
     private val dma = Dma(ppu, wRam)
     private val pad = Pad(keyEvent)
+
     private val cpuBus = CpuBus(
         ppu = ppu,
         apu = apu,
@@ -60,15 +65,21 @@ class Emulator(
         rom = prgRom,
         dma = dma,
         pad = pad
+    )
+
     private val cpu = Cpu(
         bus = cpuBus,
         interrupts = interrupts
+    )
+
     private var sleepMargin = 0L
+
     fun start() {
         while (true) {
             val frameStartNs = System.nanoTime()
             stepFrame()
             val frameEndNs = System.nanoTime()
+
             val sleepTimeNs = (FRAME_NS - (frameEndNs - frameStartNs)) + sleepMargin
             val sleepTimeMs = sleepTimeNs / 1000_000
             val sleepStartNs = System.nanoTime()
@@ -77,16 +88,25 @@ class Emulator(
             }
             val sleepEnd = System.nanoTime()
             sleepMargin = sleepTimeNs - (sleepEnd - sleepStartNs)
+        }
+    }
+
     private fun stepFrame() {
+        while (true) {
             var cpuCycle = 0
             if (dma.isProcessing) {
                 dma.run()
                 cpuCycle = 514
+            }
             cpuCycle += cpu.run()
             apu.run(cpuCycle)
             if (ppu.run(cpuCycle * 3)) {
                 break
-        // apu.flush()
+            }
+        }
+    }
+
     companion object {
         private val FRAME_NS = round(1.0 / 60 * 1000_000_000).toInt() // 60fps
+    }
 }
