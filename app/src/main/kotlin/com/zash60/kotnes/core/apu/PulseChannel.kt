4e55979b2f96386f78b5c457de85acd9f77a0ff1
Zash60 <xath06@gmail.com>
2025-11-11 11:15:43 -0300

    Update PulseChannel.kt

--- app/src/main/kotlin/com/zash60/kotnes/core/apu/PulseChannel.kt
+++ app/src/main/kotlin/com/zash60/kotnes/core/apu/PulseChannel.kt
@@ -1,21 +1,8 @@
 package com.zash60.kotnes.core.apu
 
-  import com.zash60.kotnes.core.apu.*
-  import com.zash60.kotnes.core.cartridge.*
-  import com.zash60.kotnes.core.cpu.*
-  import com.zash60.kotnes.core.dma.*
-  import com.zash60.kotnes.core.exception.*
-  import com.zash60.kotnes.core.ext.*
-  import com.zash60.kotnes.core.interrupts.*
-  import com.zash60.kotnes.core.pad.*
-  import com.zash60.kotnes.core.ppu.*
-  import com.zash60.kotnes.core.ram.*
-  import com.zash60.kotnes.core.util.*
-  import java.io.File
-  import java.math.BigInteger
-  import kotlin.math.pow
-  import kotlin.math.round
-  
+import com.zash60.kotnes.core.ext.extract
+import com.zash60.kotnes.core.ext.isSetUByte
+import com.zash60.kotnes.core.ext.update
 
 class PulseChannel(
     private val envelopeGenerator: EnvelopeGenerator,
@@ -23,6 +10,7 @@ class PulseChannel(
     isChannelOne: Boolean, // or ch. 2
 ) {
     val lengthCounterValue: Int get() = lengthCounter.value
+
     val output: UByte
         get() {
             val volume = envelopeGenerator.output
@@ -31,17 +19,24 @@ class PulseChannel(
                 lengthCounter.value == 0 ||
                 sweepUnit.isMute
             ) return 0u
+
             return (sequencerOutput * volume).toUByte()
         }
+
     private var dutyCycle: UByte = 0u
+
     private val timer = Timer(onRing = ::onTimerRing)
     private var timerPeriod = 0u
+
     private var sequenceCounter = 0
+
     private var sequencerOutput: UByte = 0u
+
     private val sweepUnit = SweepUnit(
         onTimerPeriodUpdate = { timerPeriod = it },
         useOneComplement = isChannelOne,
     )
+
     fun write(addr: Int, data: UByte) {
         when (addr) {
             0x0 -> {
@@ -49,40 +44,67 @@ class PulseChannel(
                 lengthCounter.isHalt = data.isSetUByte(5u)
                 dutyCycle = data.extract(6..7)
             }
+
             0x1 -> {
                 sweepUnit.updateRegister(data)
+            }
+
             0x2 -> {
                 timerPeriod = timerPeriod.update(data, 0..7)
+            }
+
             0x3 -> {
-                timerPeriod = timerPeriod.update(data.extract(0..2), 8..10) // TODO: is needed?
+                timerPeriod = timerPeriod.update(data.extract(0..2), 8..10)
+
                 resetSequenceCounter()
                 envelopeGenerator.setStartFlag()
                 lengthCounter.reset(data.extract(3..7).toUInt())
+            }
+        }
     }
+
     fun disable() {
         lengthCounter.disable()
+    }
+
     fun tickTimer() {
         timer.tick()
+    }
+
     fun tickEnvelope() {
         envelopeGenerator.tick()
+    }
+
     fun tickLengthCounter() {
         lengthCounter.tick()
+    }
+
     fun tickSweepUnit() {
         sweepUnit.tick(timerPeriod = timerPeriod)
+    }
+
     private fun onTimerRing(): UInt {
         sequencerOutput = runSequencer(dutyCycle = dutyCycle)
         return timerPeriod
+    }
+
     // returns 0 or 1
     private fun runSequencer(dutyCycle: UByte): UByte {
         val waveform = WAVEFORMS[dutyCycle.toInt()]
         val output = waveform[sequenceCounter].toUByte()
+
         if (sequenceCounter < 7) {
             sequenceCounter++
         } else {
             resetSequenceCounter()
+        }
         return output
+    }
+
     private fun resetSequenceCounter() {
         sequenceCounter = 0
+    }
+
     companion object {
         private val WAVEFORMS = arrayOf(
             intArrayOf(0, 1, 0, 0, 0, 0, 0, 0),
@@ -90,4 +112,5 @@ class PulseChannel(
             intArrayOf(0, 1, 1, 1, 1, 0, 0, 0),
             intArrayOf(1, 0, 0, 1, 1, 1, 1, 1),
         )
+    }
 }
