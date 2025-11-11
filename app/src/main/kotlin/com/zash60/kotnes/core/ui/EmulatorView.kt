4f7034b213fe9a68c252f0520ce4b2715098f87c
Zash60 <xath06@gmail.com>
2025-11-11 11:08:57 -0300

    Update EmulatorView.kt

--- app/src/main/kotlin/com/zash60/kotnes/core/ui/EmulatorView.kt
+++ app/src/main/kotlin/com/zash60/kotnes/core/ui/EmulatorView.kt
@@ -1,30 +1,24 @@
-package com.zash60.kotnes.core.ui
+package com.zash60.kotnes.ui
 
-  import com.zash60.kotnes.core.apu.*
-  import com.zash60.kotnes.core.cartridge.*
-  import com.zash60.kotnes.core.cpu.*
-  import com.zash60.kotnes.core.dma.*
-  import com.zash60.kotnes.core.exception.*
-  import com.zash60.kotnes.core.ext.*
-  import com.zash60.kotnes.core.interrupts.*
-  import com.zash60.kotnes.core.pad.*
-  import com.zash60.kotnes.core.ppu.*
-  import com.zash60.kotnes.core.ram.*
-  import com.zash60.kotnes.core.util.*
-  import java.io.File
-  import java.math.BigInteger
-  import kotlin.math.pow
-  import kotlin.math.round
-  
+import android.graphics.Bitmap
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
 
 // Resolução do NES
 const val SCREEN_WIDTH = 256
 const val SCREEN_HEIGHT = 240
+
 @Composable
 fun EmulatorView(bitmap: ImageBitmap) {
     Canvas(modifier = Modifier.fillMaxSize()) {
         drawImage(bitmap)
     }
 }
+
 fun createEmptyBitmap(): ImageBitmap {
     return Bitmap.createBitmap(SCREEN_WIDTH, SCREEN_HEIGHT, Bitmap.Config.ARGB_8888).asImageBitmap()
+}
