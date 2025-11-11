d5122feaef4fc6a1f7b19f35a0098347373ae63f
Zash60 <xath06@gmail.com>
2025-11-11 11:17:50 -0300

    Update TriangleChannel.kt

--- app/src/main/kotlin/com/zash60/kotnes/core/apu/TriangleChannel.kt
+++ app/src/main/kotlin/com/zash60/kotnes/core/apu/TriangleChannel.kt
@@ -1,67 +1,82 @@
 package com.zash60.kotnes.core.apu
 
-  import com.zash60.kotnes.core.apu.*
-  import com.zash60.kotnes.core.cartridge.*
-  import com.zash60.kotnes.core.cpu.*
-  import com.zash60.kotnes.core.dma.*
-  import com.zash60.kotnes.core.exception.*
-  import com.zash60.kotnes.core.ext.*
-  import com.zash60.kotnes.core.interrupts.*
-  import com.zash60.kotnes.core.pad.*
-  import com.zash60.kotnes.core.ppu.*
-  import com.zash60.kotnes.core.ram.*
-  import com.zash60.kotnes.core.util.*
-  import java.io.File
-  import java.math.BigInteger
-  import kotlin.math.pow
-  import kotlin.math.round
-  
+import com.zash60.kotnes.core.ext.extract
+import com.zash60.kotnes.core.ext.isSetUByte
+import com.zash60.kotnes.core.ext.update
 
 class TriangleChannel(private val lengthCounter: LengthCounter) {
     val lengthCounterValue: Int get() = lengthCounter.value
+
     var output: UByte = 0u
         private set
+
     private var counterReloadValue: UByte = 0u
     private var controlFlag = false
     private var timerPeriod = 0u
+
     private var timer = Timer(onRing = ::onTimerRing)
     private var sequenceIndex: UByte = 0u
+
     private var reloadLinearCounter = false
     private var linearCounter = 0
+
     fun write(addr: Int, data: UByte) {
         when (addr) {
             0x8 -> {
                 counterReloadValue = data.extract(0..6)
+
                 controlFlag = data.isSetUByte(7u)
                 lengthCounter.isHalt = controlFlag
             }
+
             // 0x9 is unused
+
             0xA -> {
                 timerPeriod = timerPeriod.update(data, 0..7)
+            }
+
             0xB -> {
                 timerPeriod = timerPeriod.update(data.extract(0..2), 8..10)
+
                 reloadLinearCounter = true
                 lengthCounter.reset(value = data.extract(3..7).toUInt())
+            }
         }
     }
+
     fun disable() {
         lengthCounter.disable()
+    }
+
     fun tickTimer() {
         timer.tick()
+    }
+
     fun tickLinearCounter() {
         if (reloadLinearCounter) {
             linearCounter = counterReloadValue.toInt()
         } else if (linearCounter > 0) {
             linearCounter--
+        }
+
         if (!controlFlag) {
             reloadLinearCounter = false
+        }
+    }
+
     fun tickLengthCounter() {
         lengthCounter.tick()
+    }
+
     private fun onTimerRing(): UInt {
         if (linearCounter > 0 && lengthCounter.value > 0) {
             output = runSequencer()
+        }
         return timerPeriod
+    }
+
     private fun runSequencer(): UByte {
         val index = (sequenceIndex and 0x1Fu).also { sequenceIndex++ }
         return if (index < 0x10u) index xor 0xFu else index and 0xFu
+    }
 }
