45d859bb3a0065f7968dd6870c3a64466c57bbed
Zash60 <xath06@gmail.com>
2025-11-11 11:09:44 -0300

    Update MainActivity.kt

--- app/src/main/kotlin/com/zash60/kotnes/MainActivity.kt
+++ app/src/main/kotlin/com/zash60/kotnes/MainActivity.kt
@@ -4,8 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-// IMPORT CORRIGIDO PARA APONTAR PARA A LOCALIZAÇÃO CORRETA DA UI
-import com.zash60.kotnes.core.ui.EmulatorView
+import com.zash60.kotnes.ui.EmulatorView
 
 class MainActivity : ComponentActivity() {
     private val viewModel: EmulatorViewModel by viewModels()
